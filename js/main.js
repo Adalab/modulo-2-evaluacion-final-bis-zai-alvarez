e93e8131ec2762383cd9d841ef9fd1527d9ca78a
zai-alvarez <zai.alvarez.l@gmail.com>
2022-04-21 14:01:18 +0200

    test

--- js/main.js
+++ js/main.js
@@ -13,8 +13,18 @@ let friends = [];
 //Pintar en el HTML los usuarios
 function paintUsers() {
     for (const user of userData) {
+        let classFriend = "";
+        const friendFoundIndex = friends.findIndex(friend => {
+            return friend.id === user;
+        });
+        if (friendFoundIndex !== -1) {
+            classFriend = "friend_marker"
+        }
+        else {
+            classFriend = "";
+        }
         list.innerHTML +=
-            `<li class="js_listUser js_liUser" id=${user.id.name}>
+            `<li class="js_listUser js_liUser ${classFriend}" id=${user.id.name}>
         <h2>"${user.name.first}"</h2>
         <h3>${user.location.city}</h3>
         <img src="${user.picture.medium}"/>
@@ -36,17 +46,37 @@ fetch(url)
 
 
 function handleClickUser(event) {
-    console.log("asdf");
     console.log(event.currentTarget.id);
+    const idUserSelected = event.currentTarget.id;  //A qué usuario le clicko
+    //---------------------------------------------//
+
+    //Para añadir a amigos
+    const userFound = userData.find(friend => { //buscar entre los usuarios
+        return friend.id === idUserSelected;
+    });
+    const friendFoundIndex = friends.findIndex(friend => { //buscar si está en el listado de favoritos
+        return friend.id === idUserSelected;
+    });
+    if (friendFoundIndex === -1) { //No lo encontró
+        friends.push(userFound); //Añádemelo
+    }
+    else { //eliminar de la lista de amigos
+        friends.splice(friendFoundIndex, 1);
+    }
+    paintUsers();
+    console.log(friends); //Este no me lo está cogiendo
 }
 
-//Escuchar a cada usuario cuando le demos click
-function listener() {
+
+//Escuchar a cada usuario cuando le demos click (función manejadora)
+function listener() {//Cojo cada li de la lista (ALL)
     const liUser = document.querySelectorAll(".js_liUser");
     console.log(liUser);
     for (const item of liUser) {
         item.addEventListener("click", handleClickUser);
     };
 };
+
+
 
 
